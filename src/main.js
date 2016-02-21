d4633e000de6075403fa7fbe1f3982e12746dc62
calmbird <mjasiszczak@gmail.com>
2016-02-22 00:59:43 +0100

    added sourcemaps

--- src/main.js
+++ src/main.js
@@ -1,14 +1,18 @@
 import 'reflect-metadata';
 import {Component} from 'angular2/core';
 import {bootstrap} from 'angular2/platform/browser';
+import {testFunction} from './test.js';
+
+testFunction('aaa');
 
 @Component({
   selector: 'app',
   template: '<p>{{ message }}</p>'
 })
+
 class egghead{
   constructor() {
-    this.message = 'cbaaaasa';
+    this.message = 'cbaaaasaas';
   }
 }
 
